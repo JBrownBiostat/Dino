011ad97543be571f4924e596d4319f2e9a53a5bb
brown46 <brown46@brown46.local>
2021-05-30 10:33:18 -0500

    Plot data formatting

---
+++ inst/script/postSimPlots.R
@@ -0,0 +1,17 @@
+#' Plot data from simulated expression
+#'
+#' These plots are used in the vignette to demonstrate the flexibility of the
+#' Dino model to smoothly estimate arbitrary latent expression distributions
+#'
+#' @docType data
+#'
+#' @usage data(postSimPlots)
+#'
+#' @format Objects of class \code{"gtable"}.
+#'
+#' @keywords datasets
+#'
+#' @examples
+#' data(postSimPlots)
+#' plot(p100_unimodal)
+"postSimPlots"
